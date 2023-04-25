ba6c90f4cda805662a930adc3115fd4bed331aa7
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-04-25 20:35:20 +0530

    main component updated

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const MainPage = () => {
   return (
-    <div>Main</div>
+    <div>
+        <div>
+            <h1>My To-Do List</h1>
+            <div id='outerDiv'>
+            <input type="text" placeholder='Work'/>
+            <button>+Add</button>
+            </div>
+        </div>
+    </div>
   )
 }
