732591b227d80c5e3031039f34cd0bdcb2d1cde6
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-04-27 17:25:29 +0530

    added click event to li

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,7 +3,6 @@ import { List } from './List';
 
 export const MainPage = () => {
     const [work,setWork] = useState("");
-    const [duWork,setDuWork] = useState("");
     const [workDetails,setWorkDetails]= useState("");
     const [todoList, setTodoList] = useState([]);
     
@@ -29,36 +28,36 @@ export const MainPage = () => {
       setWorkDetails([...workDetails,workList]);    
         
     }
+    const handleComplete =(id)=>{
+      alert("id : "+id);
+
+    }
 
     useEffect(()=>{
         localStorage.setItem('workDetails',JSON.stringify(workDetails))
     },[workDetails])
 
-    const submitbtnOnClick =(e)=> {
-        const newList  = localStorage.getItem("workDetails");
-        console.log("newList"+newList);
-        setDuWork(newList.newWork);
-
-    }
-
+   
   return (
     <div>
         <div>
             <h1>My To-Do List</h1>
             <form id='mainForm' onSubmit={handleOnSubmit}>
             <input type="text" placeholder='Work' onChange={e=>setWork(e.target.value)}value ={work}/>
-            <button type='submit' onClick={submitbtnOnClick}>+Add</button>
-            <h1>*{duWork}</h1>
-            <ul>
+            <button type='submit'>+Add</button>
+            
+            <div>
   {todoList.map((todo) => {
     return (
       <li
-      >
+        id={todo.id}
+        onClick={() => handleComplete(todo.id)}
+        >
         {todo.task}
       </li>
     );
   })}
-</ul>;
+</div>
             </form>
         </div>
     </div>
